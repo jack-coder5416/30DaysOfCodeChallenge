b287c9c4c65ac17b507a4fec11192e189dd2ca2d
Jatin pal <jackcoder137@gmail.com>
2022-01-03 19:58:12 +0530

    Day 3) Problem-1

---
+++ Find_the_position_of_Mth_item.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define max(a,b) a>b?a:b
+#define min(a,b) a<b?a:b
+#define O cout<<
+#define I cin>>
+#define f0(i,n) for(int i=0;i<n;i++)
+#define f1(i,n) for(int i=1;i<n;i++)
+#define speed_karo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll  long long
+#define lli  long long
+#define ff first
+#define ss second
+#define vecs(v) sort(v.begin(),v.end())
+#define be  v.begin(),v.end()
+int inclusion(int n,int a,int b){
+  int c1 = n/a;
+  int c2 = n/b; 
+  int c3 = n/(a*b);
+  return c1+c2-c3;  
+}
+void inputv(vector<int> &v){
+  for(auto &i : v){
+      cin>>i;
+  }
+}
+void outputv(vector<int> &v){
+  for(auto i : v){
+      cout<<i;
+  }
+}
+///-------------------------CODE BY JATIN------------------------
+ 
+ 
+void codeJ(){
+ 
+}
+int pos(int n,int m,int k){
+    if(n==1){
+        return 1;
+    }
+    int res = m%n+k-1;
+    if(res==n){
+        return res;
+    }
+    else{
+        return res%n;
+    }
+}
+//int32_t main(){
+int main(){
+int n,m,k;
+cin>>n>>m>>k;
+cout<<pos(n,m,k)<<endl;
+return 0;
+}
