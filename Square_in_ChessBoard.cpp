749c5973b0418dc643c8c78948aa3104b96de62a
Jatin pal <jackcoder137@gmail.com>
2022-01-04 18:54:26 +0530

    Day 4) Problem-1

---
+++ Square_in_ChessBoard.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define max(a,b) a>b?a:b
+#define min(a,b) a<b?a:b
+#define O cout<<
+#define I cin>>
+#define f0(i,n) for(int i=0;i<n;i++)
+#define f1(i,n) for(int i=1;i<n;i++)
+#define speed_karo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll  long long
+#define lli  long long
+#define ff first
+#define ss second
+#define vecs(v) sort(v.begin(),v.end())
+#define be  v.begin(),v.end()
+int inclusion(int n,int a,int b){
+  int c1 = n/a;
+  int c2 = n/b; 
+  int c3 = n/(a*b);
+  return c1+c2-c3;  
+}
+void inputv(vector<int> &v){
+  for(auto &i : v){
+      cin>>i;
+  }
+}
+void outputv(vector<int> &v){
+  for(auto i : v){
+      cout<<i;
+  }
+}
+///-------------------------CODE BY JATIN------------------------
+ 
+ 
+void codeJ(){
+ 
+}
+//int32_t main(){
+int main(){
+ll n;
+cin>>n;
+cout<<(n*(n+1)*(2*n+1))/6<<endl;
+return 0;
+}
