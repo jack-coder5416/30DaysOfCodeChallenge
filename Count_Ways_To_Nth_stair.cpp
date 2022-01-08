cb2c5bae8b21a20579b5383d94ebd56ba01151fe
Jatin pal <jackcoder137@gmail.com>
2022-01-08 17:34:54 +0530

    Day 8) Problem-2 (Amazon)

---
+++ Count_Ways_To_Nth_stair.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define max(a,b) a>b?a:b
+#define min(a,b) a<b?a:b
+#define O cout<<
+#define I cin>>
+#define f0(i,n) for(int i=0;i<n;i++)
+#define f1(i,n) for(int i=1;i<n;i++)
+#define speed_karo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll  long long
+#define lli  long long
+#define ff first
+#define ss second
+#define vecs(v) sort(v.begin(),v.end())
+#define be  v.begin(),v.end()
+int inclusion(int n,int a,int b){
+  int c1 = n/a;
+  int c2 = n/b; 
+  int c3 = n/(a*b);
+  return c1+c2-c3;  
+}
+void inputv(vector<int> &v){
+  for(auto &i : v){
+      cin>>i;
+  }
+}
+void outputv(vector<int> &v){
+  for(auto i : v){
+      cout<<i;
+  }
+}
+///-------------------------CODE BY JATIN------------------------
+ll countWays(ll n){
+    return n/2+1;
+} 
+ 
+void codeJ(){
+ 
+}
+//int32_t main(){
+int main(){
+
+ll n;
+cin>>n;
+cout<<countWays(n)<<endl;
+
+
+
+return 0;
+}
